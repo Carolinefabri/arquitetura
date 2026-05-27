97697c7c8a44136b1dec5ff31330834561c1c93c
Caroline de Castro Fabri <caroline.decastrofabri@aldi-sued.com>
2026-05-27 20:28:26 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Adicione esta linha com o nome EXATO do seu repositório no GitHub:
+  base: '/sap-btp-architecture-tool/', 
 })
